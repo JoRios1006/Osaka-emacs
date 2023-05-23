eb42dcfefa8d4c02c02f72787a9d511af1211834
Agus <agustinrios1006@gmail.com>
2023-05-23 00:34:25 -0300

    Added some custom QoL, no documentation yet

--- config.el
+++ config.el
@@ -13,10 +13,10 @@
   "Toggle line numbers between absolute and relative."
   (interactive)
   (if (eq display-line-numbers-type t)
-      (setq display-line-numbers-type 'relative)
-    (setq display-line-numbers-type t))
+	  (setq display-line-numbers-type 'relative)
+	(setq display-line-numbers-type t))
   (if (bound-and-true-p display-line-numbers-mode)
-      (display-line-numbers-mode)))
+	  (display-line-numbers-mode)))
 (global-set-key (kbd "C-c C-l") 'toggle-line-numbers-type)
 
 
@@ -57,6 +57,9 @@
   ("C-<tab>" . centaur-tabs-backward)
   ("C-<iso-lefttab>" . centaur-tabs-forward))
 
+;; AucTeX
+(define-key org-mode-map (kbd "C-c l c") 'org-latex-export-to-pdf)
+
 
 ;; Org-roam
 ;; A plain-text personal knowledge management system.
@@ -71,32 +74,71 @@
    ;; plain is the type of text being inserted.
    ;; "%?" is the text that will be inserted.
    ;; unnarrowed t ensures that the full file will be displayed when captured.
-    '(("d" "default" plain "%?"
-       :if-new (file+head "%<%Y-%m-%d-%H%M%S>-${slug}.org" "#+title: ${title}\n#+date: %U\n")
-       :unnarrowed t)
-     ("p" "project" plain "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
-       :if-new (file+head "%<%Y-%m-%d-%H%M%S>-${slug}.org" "#+title: ${title}\n#+filetags: Project")
-       :unnarrowed t)))
+	'(("d" "default" plain "%?"
+	   :if-new (file+head "%<%Y-%m-%d-%H%M%S>-${slug}.org" "#+title: ${title}\n#+date: %U\n")
+	   :unnarrowed t)
+	 ("p" "project" plain "* Goals\n\n%?\n\n* Tasks\n\n** TODO Add initial tasks\n\n* Dates\n\n"
+	   :if-new (file+head "%<%Y-%m-%d-%H%M%S>-${slug}.org" "#+title: ${title}\n#+filetags: Project")
+	   :unnarrowed t)))
   (org-roam-dailies-capture-templates
-    '(("d" "default" entry "* %<%H:%M>: %?"
-       :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
+	'(("d" "default" entry "* %<%H:%M>: %?"
+	   :if-new (file+head "%<%Y-%m-%d>.org" "#+title: %<%Y-%m-%d>\n"))))
   :bind (("C-c n l" . org-roam-buffer-toggle)
-         ("C-c n f" . org-roam-node-find)
-         ("C-c n i" . org-roam-node-insert)
-         ("C-c n t" . org-roam-tag-add)
-         ("C-c n a" . org-roam-alias-add)
-         ("C-c n o" . org-id-get-create)
-         :map org-mode-map
-         ("C-M-i" . completion-at-point)
-         :map org-roam-dailies-map
-         ("Y" . org-roam-dailies-capture-yesterday)
-         ("T" . org-roam-dailies-capture-tomorrow))
+		 ("C-c n f" . org-roam-node-find)
+		 ("C-c n i" . org-roam-node-insert)
+		 ("C-c n t" . org-roam-tag-add)
+		 ("C-c n a" . org-roam-alias-add)
+		 ("C-c n o" . org-id-get-create)
+		 :map org-mode-map
+		 ("C-M-i" . completion-at-point)
+		 :map org-roam-dailies-map
+		 ("Y" . org-roam-dailies-capture-yesterday)
+		 ("T" . org-roam-dailies-capture-tomorrow))
   :bind-keymap
   ("C-c n d" . org-roam-dailies-map)
   :config
   (org-roam-setup)
   (require 'org-roam-dailies) ;; Ensure the keymap is available
   (org-roam-db-autosync-mode))
+
+;; Crux: Collection of Ridiculously Useful eXtensions
+(use-package crux
+  :ensure t)
+(global-set-key [remap move-beginning-of-line] #'crux-move-beginning-of-line)
+(global-set-key [remap kill-whole-line] #'crux-kill-whole-line)
+(global-set-key (kbd "C-S-RET") 'crux-smart-open-line-above)
+(global-set-key (kbd "S-RET") 'crux-smart-open-line)
+(global-set-key (kbd "C-c k") 'crux-kill-other-buffers)
+(global-set-key (kbd "C-c t") 'crux-visit-term-buffer)
+(global-set-key (kbd "C-c I") 'crux-find-user-init-file)
+(global-set-key (kbd "M-o") 'crux-other-window-or-switch-buffer)
+(global-set-key (kbd "C-c c b") 'crux-cleanup-buffer-or-region)
+(global-set-key (kbd "C-c e") 'crux-eval-and-replace)
+(global-set-key (kbd "C-c D") 'crux-delete-file-and-buffer)
+
+;; Multiple Cursors
+(use-package multiple-cursors
+  :ensure t)
+(global-set-key (kbd "C-c x") 'mc/mark-next-like-this)
+(define-key mc/keymap (kbd "<return>") nil)
+
+;; TLDR Pages
+(use-package tldr
+  :ensure t)
+
+;; Projectile
+(use-package projectile
+  :ensure t)
+(projectile-mode +1)
+(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+
+;; SLY is Sylvester the Cat's Common Lisp IDE for Emacs
+(use-package sly
+  :ensure t)
+(use-package common-lisp-snippets
+  :ensure t)
+
+;; TODO lsp-mode lsp-ui
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (show-paren-mode 1)
@@ -126,25 +168,25 @@
 (global-prettify-symbols-mode t)
 
 (setq electric-pair-pairs '(
-                            (?\{ . ?\})
-                            (?\( . ?\))
-                            (?\[ . ?\])
-                            (?\" . ?\")
-                            ))
+							(?\{ . ?\})
+							(?\( . ?\))
+							(?\[ . ?\])
+							(?\" . ?\")
+							))
 (electric-pair-mode t)
 
 (defun split-and-follow-horizontally ()
-      (interactive)
-      (split-window-below)
-      (balance-windows)
-      (other-window 1))
+	  (interactive)
+	  (split-window-below)
+	  (balance-windows)
+	  (other-window 1))
 (global-set-key (kbd "C-x 2") 'split-and-follow-horizontally)
 
 (defun split-and-follow-vertically ()
-      (interactive)
-      (split-window-right)
-      (balance-windows)
-      (other-window 1))
+	  (interactive)
+	  (split-window-right)
+	  (balance-windows)
+	  (other-window 1))
 (global-set-key (kbd "C-x 3") 'split-and-follow-vertically)
 
 (defalias 'yes-or-no-p 'y-or-n-p)
@@ -162,8 +204,8 @@
   :config
   (add-hook 'org-mode-hook 'org-indent-mode)
   (add-hook 'org-mode-hook
-            #'(lambda ()
-               (visual-line-mode 1))))
+			#'(lambda ()
+			   (visual-line-mode 1))))
 
 (use-package org-indent
   :diminish org-indent-mode)
@@ -173,17 +215,17 @@
 
 (setq eshell-prompt-regexp "^[^αλ\n]*[αλ] ")
 (setq eshell-prompt-function
-      (lambda nil
-        (concat
-         (if (string= (eshell/pwd) (getenv "HOME"))
-             (propertize "~" 'face `(:foreground "#99CCFF"))
-           (replace-regexp-in-string
-            (getenv "HOME")
-            (propertize "~" 'face `(:foreground "#99CCFF"))
-            (propertize (eshell/pwd) 'face `(:foreground "#99CCFF"))))
-         (if (= (user-uid) 0)
-             (propertize " α " 'face `(:foreground "#FF6666"))
-         (propertize " λ " 'face `(:foreground "#A6E22E"))))))
+	  (lambda nil
+		(concat
+		 (if (string= (eshell/pwd) (getenv "HOME"))
+			 (propertize "~" 'face `(:foreground "#99CCFF"))
+		   (replace-regexp-in-string
+			(getenv "HOME")
+			(propertize "~" 'face `(:foreground "#99CCFF"))
+			(propertize (eshell/pwd) 'face `(:foreground "#99CCFF"))))
+		 (if (= (user-uid) 0)
+			 (propertize " α " 'face `(:foreground "#FF6666"))
+		 (propertize " λ " 'face `(:foreground "#A6E22E"))))))
 
 (setq eshell-highlight-prompt nil)
 
@@ -193,21 +235,21 @@
 (defun eshell/sudo-open (filename)
   "Open a file as root in Eshell."
   (let ((qual-filename (if (string-match "^/" filename)
-                           filename
-                         (concat (expand-file-name (eshell/pwd)) "/" filename))))
-    (switch-to-buffer
-     (find-file-noselect
-      (concat "/sudo::" qual-filename)))))
+						   filename
+						 (concat (expand-file-name (eshell/pwd)) "/" filename))))
+	(switch-to-buffer
+	 (find-file-noselect
+	  (concat "/sudo::" qual-filename)))))
 
 (defun eshell-other-window ()
   "Create or visit an eshell buffer."
   (interactive)
   (if (not (get-buffer "*eshell*"))
-      (progn
-        (split-window-sensibly (selected-window))
-        (other-window 1)
-        (eshell))
-    (switch-to-buffer-other-window "*eshell*")))
+	  (progn
+		(split-window-sensibly (selected-window))
+		(other-window 1)
+		(eshell))
+	(switch-to-buffer-other-window "*eshell*")))
 
 (global-set-key (kbd "<s-C-return>") 'eshell-other-window)
 
@@ -231,7 +273,7 @@
 ;    `(progn
 ;       ,@(cl-loop for (mode . file) in zip-diminish
 ;		  collect `(with-eval-after-load ,file
-;			     (diminish (quote ,mode)))))))
+;				 (diminish (quote ,mode)))))))
 ; This bit goes in init.el
 ;(diminish-built-in
 ;  beacon-mode
@@ -248,33 +290,33 @@
   :ensure t)
 
 (use-package powerline
-      :ensure t
-      :init
-      (spaceline-spacemacs-theme)
-      :hook
-      ('after-init-hook) . 'powerline-reset)
+	  :ensure t
+	  :init
+	  (spaceline-spacemacs-theme)
+	  :hook
+	  ('after-init-hook) . 'powerline-reset)
 
 (use-package dashboard
   :ensure t
   :defer nil
   :preface
   (defun update-config ()
-    "Update Witchmacs to the latest version."
-    (interactive)
-    (let ((dir (expand-file-name user-emacs-directory)))
-      (if (file-exists-p dir)
-          (progn
-            (message "Witchmacs is updating!")
-            (cd dir)
-            (shell-command "git pull")
-            (message "Update finished. Switch to the messages buffer to see changes and then restart Emacs"))
-        (message "\"%s\" doesn't exist." dir))))
+	"Update Witchmacs to the latest version."
+	(interactive)
+	(let ((dir (expand-file-name user-emacs-directory)))
+	  (if (file-exists-p dir)
+		  (progn
+			(message "Witchmacs is updating!")
+			(cd dir)
+			(shell-command "git pull")
+			(message "Update finished. Switch to the messages buffer to see changes and then restart Emacs"))
+		(message "\"%s\" doesn't exist." dir))))
 
   (defun create-scratch-buffer ()
-    "Create a scratch buffer"
-    (interactive)
-    (switch-to-buffer (get-buffer-create "*scratch*"))
-    (lisp-interaction-mode))
+	"Create a scratch buffer"
+	(interactive)
+	(switch-to-buffer (get-buffer-create "*scratch*"))
+	(lisp-interaction-mode))
   :config
   (dashboard-setup-startup-hook)
   (setq dashboard-items '((recents . 5)))
@@ -284,38 +326,38 @@
   (setq dashboard-show-shortcuts nil)
   (setq dashboard-set-init-info t)
   (setq dashboard-init-info (format "%d packages loaded in %s"
-                                    (length package-activated-list) (emacs-init-time)))
+									(length package-activated-list) (emacs-init-time)))
   (setq dashboard-set-footer nil)
   (setq dashboard-set-navigator t)
   (setq dashboard-navigator-buttons
-        `(;; line1
-          ((,nil
-            "Witchmacs on github"
-            "Open Witchmacs' github page on your browser"
-            (lambda (&rest _) (browse-url "https://github.com/snackon/witchmacs"))
-            'default)
-           (nil
-            "Witchmacs crash course"
-            "Open Witchmacs' introduction to Emacs"
-            (lambda (&rest _) (find-file "~/.emacs.d/Witcheat.org"))
-            'default)
-           (nil
-            "Update Witchmacs"
-            "Get the latest Witchmacs update. Check out the github commits for changes!"
-            (lambda (&rest _) (update-config))
-            'default)
-           )
-          ;; line 2
-          ((,nil
-            "Open scratch buffer"
-            "Switch to the scratch buffer"
-            (lambda (&rest _) (create-scratch-buffer))
-            'default)
-           (nil
-            "Open config.org"
-            "Open Witchmacs' configuration file for easy editing"
-            (lambda (&rest _) (find-file "~/.emacs.d/config.org"))
-            'default)))))
+		`(;; line1
+		  ((,nil
+			"Witchmacs on github"
+			"Open Witchmacs' github page on your browser"
+			(lambda (&rest _) (browse-url "https://github.com/snackon/witchmacs"))
+			'default)
+		   (nil
+			"Witchmacs crash course"
+			"Open Witchmacs' introduction to Emacs"
+			(lambda (&rest _) (find-file "~/.emacs.d/Witcheat.org"))
+			'default)
+		   (nil
+			"Update Witchmacs"
+			"Get the latest Witchmacs update. Check out the github commits for changes!"
+			(lambda (&rest _) (update-config))
+			'default)
+		   )
+		  ;; line 2
+		  ((,nil
+			"Open scratch buffer"
+			"Switch to the scratch buffer"
+			(lambda (&rest _) (create-scratch-buffer))
+			'default)
+		   (nil
+			"Open config.org"
+			"Open Witchmacs' configuration file for easy editing"
+			(lambda (&rest _) (find-file "~/.emacs.d/config.org"))
+			'default)))))
 
 ;(insert (concat
 ;         (propertize (format "%d packages loaded in %s"
@@ -331,8 +373,8 @@
   (which-key-mode))
 
 (use-package swiper
-      :ensure t
-      :bind ("C-s" . 'swiper))
+	  :ensure t
+	  :bind ("C-s" . 'swiper))
 
 (use-package evil
   :ensure t
@@ -356,21 +398,21 @@
   (beacon-mode 1))
 
 (use-package avy
-      :ensure t
-      :bind
-      ("M-s" . avy-goto-char))
+	  :ensure t
+	  :bind
+	  ("M-s" . avy-goto-char))
 
 (use-package switch-window
-      :ensure t
-      :config
-      (setq switch-window-input-style 'minibuffer)
-      (setq switch-window-increase 4)
-      (setq switch-window-threshold 2)
-      (setq switch-window-shortcut-style 'qwerty)
-      (setq switch-window-qwerty-shortcuts
+	  :ensure t
+	  :config
+	  (setq switch-window-input-style 'minibuffer)
+	  (setq switch-window-increase 4)
+	  (setq switch-window-threshold 2)
+	  (setq switch-window-shortcut-style 'qwerty)
+	  (setq switch-window-qwerty-shortcuts
 		'("a" "s" "d" "f" "j" "k" "l"))
-      :bind
-      ([remap other-window] . switch-window))
+	  :bind
+	  ([remap other-window] . switch-window))
 
 (use-package ido
   :init
@@ -390,9 +432,9 @@
 (setq ido-vertical-define-keys 'C-n-C-p-up-and-down)
 
 (use-package async
-      :ensure t
-      :init
-      (dired-async-mode 1))
+	  :ensure t
+	  :init
+	  (dired-async-mode 1))
 
 (use-package page-break-lines
   :ensure t
@@ -407,69 +449,69 @@
   :defer t
   :init
   (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+	(define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
   :config
   (progn
-    (setq treemacs-collapse-dirs                 (if (executable-find "python3") 3 0)
-          treemacs-deferred-git-apply-delay      0.5
-          treemacs-display-in-side-window        t
-          treemacs-eldoc-display                 t
-          treemacs-file-event-delay              5000
-          treemacs-file-follow-delay             0.2
-          treemacs-follow-after-init             t
-          treemacs-git-command-pipe              ""
-          treemacs-goto-tag-strategy             'refetch-index
-          treemacs-indentation                   2
-          treemacs-indentation-string            " "
-          treemacs-is-never-other-window         nil
-          treemacs-max-git-entries               5000
-          treemacs-missing-project-action        'ask
-          treemacs-no-png-images                 nil
-          treemacs-no-delete-other-windows       t
-          treemacs-project-follow-cleanup        nil
-          treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-recenter-distance             0.1
-          treemacs-recenter-after-file-follow    nil
-          treemacs-recenter-after-tag-follow     nil
-          treemacs-recenter-after-project-jump   'always
-          treemacs-recenter-after-project-expand 'on-distance
-          treemacs-show-cursor                   nil
-          treemacs-show-hidden-files             t
-          treemacs-silent-filewatch              nil
-          treemacs-silent-refresh                nil
-          treemacs-sorting                       'alphabetic-desc
-          treemacs-space-between-root-nodes      t
-          treemacs-tag-follow-cleanup            t
-          treemacs-tag-follow-delay              1.5
-          treemacs-width                         30)
-    (treemacs-resize-icons 11)
+	(setq treemacs-collapse-dirs                 (if (executable-find "python3") 3 0)
+		  treemacs-deferred-git-apply-delay      0.5
+		  treemacs-display-in-side-window        t
+		  treemacs-eldoc-display                 t
+		  treemacs-file-event-delay              5000
+		  treemacs-file-follow-delay             0.2
+		  treemacs-follow-after-init             t
+		  treemacs-git-command-pipe              ""
+		  treemacs-goto-tag-strategy             'refetch-index
+		  treemacs-indentation                   2
+		  treemacs-indentation-string            " "
+		  treemacs-is-never-other-window         nil
+		  treemacs-max-git-entries               5000
+		  treemacs-missing-project-action        'ask
+		  treemacs-no-png-images                 nil
+		  treemacs-no-delete-other-windows       t
+		  treemacs-project-follow-cleanup        nil
+		  treemacs-persist-file                  (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+		  treemacs-recenter-distance             0.1
+		  treemacs-recenter-after-file-follow    nil
+		  treemacs-recenter-after-tag-follow     nil
+		  treemacs-recenter-after-project-jump   'always
+		  treemacs-recenter-after-project-expand 'on-distance
+		  treemacs-show-cursor                   nil
+		  treemacs-show-hidden-files             t
+		  treemacs-silent-filewatch              nil
+		  treemacs-silent-refresh                nil
+		  treemacs-sorting                       'alphabetic-desc
+		  treemacs-space-between-root-nodes      t
+		  treemacs-tag-follow-cleanup            t
+		  treemacs-tag-follow-delay              1.5
+		  treemacs-width                         30)
+	(treemacs-resize-icons 11)
 
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode t)
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null (executable-find "python3"))))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple))))
+	(treemacs-follow-mode t)
+	(treemacs-filewatch-mode t)
+	(treemacs-fringe-indicator-mode t)
+	(pcase (cons (not (null (executable-find "git")))
+				 (not (null (executable-find "python3"))))
+	  (`(t . t)
+	   (treemacs-git-mode 'deferred))
+	  (`(t . _)
+	   (treemacs-git-mode 'simple))))
   :bind
   (:map global-map
-        ("M-0"       . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
+		("M-0"       . treemacs-select-window)
+		("C-x t 1"   . treemacs-delete-other-windows)
+		("C-x t t"   . treemacs)
+		("C-x t B"   . treemacs-bookmark)
+		("C-x t C-t" . treemacs-find-file)
+		("C-x t M-t" . treemacs-find-tag)))
 
 (use-package treemacs-evil
   :after treemacs evil
-    :ensure t)
+	:ensure t)
 
   (use-package treemacs-icons-dired
-    :after treemacs dired
-    :ensure t
-    :config (treemacs-icons-dired-mode))
+	:after treemacs dired
+	:ensure t
+	:config (treemacs-icons-dired-mode))
 
 (use-package magit
   :ensure t)
@@ -514,8 +556,8 @@
   :ensure t
   :config
   (setq company-backends '((company-c-headers
-                            company-dabbrev-code
-                            company-irony))))
+							company-dabbrev-code
+							company-irony))))
 (use-package irony
   :defer nil
   :ensure t
@@ -523,16 +565,4 @@
   :hook
   ((c++-mode c-mode) . irony-mode)
   ('irony-mode-hook) . 'irony-cdb-autosetup-compile-options)
-
-(use-package meghanada
-  :ensure t
-  :defer nil
-  :config
-  (add-hook 'java-mode-hook
-            (lambda ()
-              (meghanada-mode t)))
-  (setq meghanada-java-path "java")
-  (setq meghanada-maven-path "mvn"))
-
-
 
--- init.el
+++ init.el
@@ -38,11 +38,95 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(connection-local-criteria-alist
+   '(((:application eshell)
+	  eshell-connection-default-profile)
+	 ((:application tramp :protocol "flatpak")
+	  tramp-container-connection-local-default-flatpak-profile)
+	 ((:application tramp)
+	  tramp-connection-local-default-system-profile tramp-connection-local-default-shell-profile)))
+ '(connection-local-profile-alist
+   '((eshell-connection-default-profile
+	  (eshell-path-env-list))
+	 (tramp-container-connection-local-default-flatpak-profile
+	  (tramp-remote-path "/app/bin" tramp-default-remote-path "/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin" "/local/bin" "/local/freeware/bin" "/local/gnu/bin" "/usr/freeware/bin" "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin" "/opt/sbin" "/opt/local/bin"))
+	 (tramp-connection-local-darwin-ps-profile
+	  (tramp-process-attributes-ps-args "-acxww" "-o" "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state=abcde" "-o" "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
+	  (tramp-process-attributes-ps-format
+	   (pid . number)
+	   (euid . number)
+	   (user . string)
+	   (egid . number)
+	   (comm . 52)
+	   (state . 5)
+	   (ppid . number)
+	   (pgrp . number)
+	   (sess . number)
+	   (ttname . string)
+	   (tpgid . number)
+	   (minflt . number)
+	   (majflt . number)
+	   (time . tramp-ps-time)
+	   (pri . number)
+	   (nice . number)
+	   (vsize . number)
+	   (rss . number)
+	   (etime . tramp-ps-time)
+	   (pcpu . number)
+	   (pmem . number)
+	   (args)))
+	 (tramp-connection-local-busybox-ps-profile
+	  (tramp-process-attributes-ps-args "-o" "pid,user,group,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "stat=abcde" "-o" "ppid,pgid,tty,time,nice,etime,args")
+	  (tramp-process-attributes-ps-format
+	   (pid . number)
+	   (user . string)
+	   (group . string)
+	   (comm . 52)
+	   (state . 5)
+	   (ppid . number)
+	   (pgrp . number)
+	   (ttname . string)
+	   (time . tramp-ps-time)
+	   (nice . number)
+	   (etime . tramp-ps-time)
+	   (args)))
+	 (tramp-connection-local-bsd-ps-profile
+	  (tramp-process-attributes-ps-args "-acxww" "-o" "pid,euid,user,egid,egroup,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state,ppid,pgid,sid,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etimes,pcpu,pmem,args")
+	  (tramp-process-attributes-ps-format
+	   (pid . number)
+	   (euid . number)
+	   (user . string)
+	   (egid . number)
+	   (group . string)
+	   (comm . 52)
+	   (state . string)
+	   (ppid . number)
+	   (pgrp . number)
+	   (sess . number)
+	   (ttname . string)
+	   (tpgid . number)
+	   (minflt . number)
+	   (majflt . number)
+	   (time . tramp-ps-time)
+	   (pri . number)
+	   (nice . number)
+	   (vsize . number)
+	   (rss . number)
+	   (etime . number)
+	   (pcpu . number)
+	   (pmem . number)
+	   (args)))
+	 (tramp-connection-local-default-shell-profile
+	  (shell-file-name . "/bin/sh")
+	  (shell-command-switch . "-c"))
+	 (tramp-connection-local-default-system-profile
+	  (path-separator . ":")
+	  (null-device . "/dev/null"))))
  '(custom-enabled-themes '(Witchmacs))
  '(custom-safe-themes
    '("4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "acb636fb88d15c6dd4432e7f197600a67a48fd35b54e82ea435d7cd52620c96d" default))
  '(package-selected-packages
-   '(centaur-tabs rainbow-delimiters rainbow-mode highlight-numbers org-roam color-theme-sanityinc-solarized meghanada company-irony company-c-headers yasnippet-snippets yasnippet company magit treemacs-icons-dired treemacs-evil treemacs undo-tree page-break-lines async ido-vertical-mode switch-window avy beacon evil swiper which-key dashboard spaceline diminish auto-package-update htmlize)))
+   '(common-lisp-snippets sly sly-mode sunrise projectile tldr tldr.el multiple-cursors crux auctex centaur-tabs rainbow-delimiters rainbow-mode highlight-numbers org-roam color-theme-sanityinc-solarized meghanada company-irony company-c-headers yasnippet-snippets yasnippet company magit treemacs-icons-dired treemacs-evil treemacs undo-tree page-break-lines async ido-vertical-mode switch-window avy beacon evil swiper which-key dashboard spaceline diminish auto-package-update htmlize)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
